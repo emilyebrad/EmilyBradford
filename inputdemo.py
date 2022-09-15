a8227c85c10f6a081ba63f2e440c712a46d5d674
Bradford <E1BRADFORD@student.bridgew.edu>
2022-09-15 12:01:05 -0400

    Initial commit

---
+++ inputdemo.py
@@ -0,0 +1,3 @@
+your_name = input("Tell me your name:")
+major = input("What is your major:")
+print("Hello", your_name, "this comp151 class will help you with", major)
---
+++ main.py
@@ -0,0 +1,4 @@
+message = "Hello 11am Comp151 :)"
+print(message)
+message = "to all those enthusiastic students!"
+print(message)
